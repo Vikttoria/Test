4b56010b5e7a376c430456483fada7e733798315
Vikttoria <77542065+Vikttoria@users.noreply.github.com>
2021-07-10 16:32:10 +0300

    Add files via upload

---
+++ phpM/index.js
@@ -0,0 +1,15 @@
+
+    var n = 50;
+    result=0;
+        result += (n % 10) * (n % 10);
+        result = parseInt(n/10);
+        console.log(result);
+       if(result==1){
+       console.log("happy") ;
+        }else{
+             console.log("noy") ;
+        }
+   
+        //console.log (result == 1 ? "happy" : "noy" );
+
+
